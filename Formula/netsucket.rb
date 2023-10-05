0ceb786a5c7ef2230655ed4bc38f5d2ddb6abe95
Constantine Kurianoff <kurianoff@gmail.com>
2023-10-05 01:27:52 -0400

    chore: upgrade from v0.0.5 to v0.0.6

--- Formula/netsucket.rb
+++ Formula/netsucket.rb
@@ -1,15 +1,15 @@
 class Netsucket < Formula
   desc "Port monitoring tool that proxies traffic between a client and the tunnel"
   homepage "https://github.com/kurianoff/netsucket"
-  version "0.0.5"
+  version "0.0.6"
   license "MIT"
 
   if Hardware::CPU.arm?
     url "https://github.com/kurianoff/netsucket/releases/download/v#{version}/netsucket-darwin-arm64"
-    sha256 "b5dda0a1d702e38efb515b469f47cc6dddd5be9b56631720b5cb29fa4b7d44e5"
+    sha256 "8b3f457dd91fb8cb7f8d68e28a09e542c732846e07c0d946e1a7e0511512d21d"
   else
     url "https://github.com/kurianoff/netsucket/releases/download/v#{version}/netsucket-darwin-amd64"
-    sha256 "b6ca8f983a301c0c0b268dc3ea02ad46d5caa2a1e05ef525786022047a7606d9"
+    sha256 "eaebdc0989449f1538c803dacd3799a2929a3156885ab41d10c02ba44f330f13"
   end
 
   def install
