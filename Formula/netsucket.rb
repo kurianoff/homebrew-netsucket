e8a6ce3ded5a150e2f204b8f0913dc45ed61ef0c
Constantine Kurianoff <kurianoff@gmail.com>
2023-10-05 00:15:31 -0400

    add sha256

--- Formula/netsucket.rb
+++ Formula/netsucket.rb
@@ -6,10 +6,10 @@ class Netsucket < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/kurianoff/netsucket/releases/download/v#{version}/netsucket-darwin-arm64"
-    sha256 "YOUR_ARM64_SHA256_CHECKSUM_HERE"
+    sha256 "34b408be8557907ca6857788da9fa3530dcc0e02c04ea6b127729d1881def150"
   else
     url "https://github.com/kurianoff/netsucket/releases/download/v#{version}/netsucket-darwin-amd64"
-    sha256 "YOUR_AMD64_SHA256_CHECKSUM_HERE"
+    sha256 "f27202a7449a153b463f9de698153b2b7c1a2d3bc8b6aaeae0274a131cf5236f"
   end
 
   def install
